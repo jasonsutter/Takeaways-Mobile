4ba07fd6c3f99acba6c019d212e81afe815c0619
Jason Sutter <jason@animascodelabs.com>
2014-10-14 15:45:08 -0600

    Created App_delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame UIScreen.mainScreen.bounds
+
+    bvc = BrochureRacksViewController.alloc.init
+    nav = UINavigationController.alloc.initWithRootViewController bvc
+
+    @window.rootViewController = nav
+    @window.makeKeyAndVisible
     true
   end
 end
